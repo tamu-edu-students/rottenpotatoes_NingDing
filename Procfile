7cca9f237956db4f4c7daf5ebea5eba0eb8e2bb6
ningding <dingning@tamu.edu>
2022-10-29 04:04:30 -0500

    some changes

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 web: bundle exec rails server -p $PORT
-heroku run rake db:migrate
-heroku run rake db:seed
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  {:title => 'Star wars', :rating => 'B',
+    :release_date => '16-Apr-1977'},
+ # {:title => 'Green Book', :rating => 'PG-13',
+  #  :release_date => '16-Nov-2018'},
+  #{:title => 'Parasite', :rating => 'R',
+   # :release_date => '30-May-2019'},
+  #{:title => 'Nomadland', :rating => 'R',
+   # :release_date => '19-Feb-2021'},
+  #{:title => 'CODA', :rating => 'PG-13',
+   # :release_date => '13-Aug-2021'}
 ]
 
 more_movies.each do |movie|
